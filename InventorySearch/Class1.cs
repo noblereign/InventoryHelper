ce67638176153faa262605e3d34e83c63b531779
Noble <noblereign@users.noreply.github.com>
2025-11-23 05:01:52 -0800

    Cleanup for .NET 9
    Change csproj format and remove some unused usings

--- InventorySearch/Class1.cs
+++ InventorySearch/Class1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Elements.Assets;
 using Elements.Core;
 using Newtonsoft.Json;
 using FrooxEngine;
